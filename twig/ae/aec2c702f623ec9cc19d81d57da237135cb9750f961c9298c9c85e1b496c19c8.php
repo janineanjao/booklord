ba024dd98a6f6efd06659888e03f5511c9ac7b37
janineanjao <60459207+janineanjao@users.noreply.github.com>
2020-03-21 04:20:57 -0700

    Add files via upload

---
+++ twig/ae/aec2c702f623ec9cc19d81d57da237135cb9750f961c9298c9c85e1b496c19c8.php
@@ -0,0 +1,64 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* @Framework/Form/button_attributes.html.php */
+class __TwigTemplate_dffe4e6c52388e1e8765849a0ae887a35aab1235742344aa0125bfc99b3b12f2 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "@Framework/Form/button_attributes.html.php"));
+
+        // line 1
+        echo "id=\"<?php echo \$view->escape(\$id) ?>\" name=\"<?php echo \$view->escape(\$full_name) ?>\"<?php if (\$disabled): ?> disabled=\"disabled\"<?php endif ?>
+<?php echo \$attr ? ' '.\$view['form']->block(\$form, 'attributes') : '' ?>
+";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "@Framework/Form/button_attributes.html.php";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  40 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("id=\"<?php echo \$view->escape(\$id) ?>\" name=\"<?php echo \$view->escape(\$full_name) ?>\"<?php if (\$disabled): ?> disabled=\"disabled\"<?php endif ?>
+<?php echo \$attr ? ' '.\$view['form']->block(\$form, 'attributes') : '' ?>
+", "@Framework/Form/button_attributes.html.php", "C:\\Users\\Cherry Anne\\booklord\\vendor\\symfony\\framework-bundle\\Resources\\views\\Form\\button_attributes.html.php");
+    }
+}
